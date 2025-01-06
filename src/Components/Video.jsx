4a39066a8b85dacb5f56f4989f5a7366315ef811
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2025-01-06 22:42:50 +0600

    added dynamic button at Immersive Adventures section

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import cod from "../../public/COD.mp4";
 
 const Video = () => {
@@ -6,7 +6,7 @@ const Video = () => {
     <div className="relative h-screen w-full overflow-hidden max-w-screen-2xl mx-auto">
       <div className="relative w-full h-full">
         <video
-          src={cod} 
+          src={cod}
           autoPlay
           muted
           loop
@@ -15,22 +15,22 @@ const Video = () => {
         />
       </div>
 
-
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black/50 px-4 text-center">
-
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white dark:text-gray-100 leading-tight">
           Immersive Adventures Await
         </h1>
 
-
         <p className="mt-4 text-base md:text-lg lg:text-xl text-gray-300 dark:text-gray-400 max-w-md md:max-w-lg lg:max-w-2xl">
-          Click here to download the game and embark on an unforgettable gaming journey!
+          Click here to download the game and embark on an unforgettable gaming
+          journey!
         </p>
 
-   
-        <button className="mt-6 px-6 py-3 font-bold bg-gradient-to-r from-[#00ADB5] to-[#008C8C] text-white hover:bg-gradient-to-l hover:bg-[#008C8C]  duration-300 border-none rounded-lg hover:scale-105 transition-transform text-sm md:text-base lg:text-lg">
-          Download Now!
-        </button>
+        <Link to="/allreviews">
+          {" "}
+          <button className="mt-6 px-6 py-3 font-bold bg-gradient-to-r from-[#00ADB5] to-[#008C8C] text-white hover:bg-gradient-to-l hover:bg-[#008C8C]  duration-300 border-none rounded-lg hover:scale-105 transition-transform text-sm md:text-base lg:text-lg">
+            Explore All Reviews
+          </button>
+        </Link>
       </div>
     </div>
   );
